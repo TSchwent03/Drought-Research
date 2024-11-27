9f4caaa45d928f00a593eaf0e0477b7f5a3b7df1
TSchwent03 <thomasschwentWX@gmail.com>
2024-11-26 18:19:11 -0600

    Duration Maps Complete
    Average, Cumulative, and Extremes Drought Duration Maps are complete
    Drought seasonality maps are up next!

--- Programs/Seasonality Maps.py
+++ Programs/Seasonality Maps.py
@@ -16,7 +16,7 @@ api_key = "***REMOVED***"
 gmaps = googlemaps.Client(key=api_key)
 geoloc = pd.read_csv(r"C:\Users\thoma\Documents\GitHub\Drought-Research\output.csv")
 
-def create_duration_dataframe(input_dir, spi_time, drought_threshold):
+def create_seasonality_dataframe(input_dir, spi_time, drought_threshold):
     """
     Creates a DataFrame of drought durations for a given SPI time scale and threshold.
 
@@ -60,71 +60,19 @@ def create_duration_dataframe(input_dir, spi_time, drought_threshold):
             # Identify drought events and calculate durations
             drought_start = None
             for index, row in df.iterrows():
-                if row[spi_row] <= drought_threshold:
+                if df[spi_row] <= drought_threshold:
                     if drought_start is None:
-                        drought_start = index
+                        drought_start = row['date']
                 else:
                     if drought_start is not None:
-                        drought_end = index
-                        duration = drought_end - drought_start
+                        drought_end = row['date']
+                        duration = (drought_end - drought_start).days
                         duration_data.append({'location': location, 'SPI': drought_threshold, 'start_date': drought_start, 'end_date': drought_end, 'duration': duration})
                         drought_start = None
 
     # Create DataFrame
     duration_df = pd.DataFrame(duration_data)
 
-    # Group the DataFrame by location and calculate the average duration
-    cumulative_durations = duration_df.groupby('location')['duration'].min().reset_index()
+    return duration_df
 
-    return cumulative_durations
-
-def duration_map_plot(spi_key, spi_time):
-
-    # Load shapefile
-    directory_path = rf"C:\Users\thoma\Documents\GitHub\Drought-Research\Maps\SPI Maps - V1\Shortest Duration\\{spi_time}M"
-    shapefile_path = r"C:\Users\thoma\Documents\GitHub\Drought-Research\MO_County_Boundaries.shp"
-    counties = gpd.read_file(shapefile_path)
-    counties_crs = counties.to_crs("EPSG:4326")
-    duration_df = create_duration_dataframe(input_dir, spi_time, spi_key)
-    geoloc_call = pd.concat([duration_df, geoloc['latitude'], geoloc['longitude']], axis=1, join='outer')
-    geoloc_call['county_name'] = duration_df['location'].apply(lambda x: re.sub(r' County$', '', x.split(',')[1]).strip())
-    # Extract latitude and longitude from the latlon column
-    geoloc_gdf = gpd.GeoDataFrame(geoloc_call, geometry=gpd.points_from_xy(geoloc_call['longitude'], geoloc_call['latitude']))
-
-    # Create figure and axes
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    # Plot the map
-    counties_crs.plot(ax=ax, color='lightgray', edgecolor='black')
-    # Plots Graduated Color Mapping
-    geoloc_gdf.plot(ax=ax ,column='duration', cmap='cividis', legend=True, markersize=75, edgecolor='black', linewidth=1)
-
-    # Add custom legend labels
-    plt.xlabel("Longitude")
-    plt.ylabel("Latitude")
-    plt.title(f'Shortest Drought Duration (Spi Key: {spi_key}, SPI Time: {spi_time})', fontsize= 11)
-
-    # Create the filename
-    filename = os.path.join(directory_path, f"spi_map_{spi_key}_{spi_time}.jpg")
-
-    # Save the plot
-    plt.savefig(filename)
-    plt.close()
-    #plt.show()
-
-def duration_map_loop(SPI_time):
-    spi_key = 00
-
-    try:
-        # Iterate through possible SPI values
-        while spi_key != -51:
-            # Call the frequency_map_plot function
-            duration_map_plot(spi_key/10, SPI_time)
-            spi_key -= 1
-    except:
-        return None
-
-duration_map_loop('01')
-duration_map_loop('03')
-duration_map_loop('06')
-duration_map_loop('12')
+print(create_seasonality_dataframe(input_dir, '03', -0.5))
